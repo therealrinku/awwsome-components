58865ee2acc3ebe0862b393f2ffcccafc5ef9ee7
therealrinku <rinkunited2032@gmail.com>
2024-03-08 17:19:16 +0545

    docs: ts documentations added

--- core/fb.ts
+++ core/fb.ts
@@ -1,30 +1,51 @@
 /*
   Socials.js @2024
-  
+
   FB Graph API SDK
   - long lived access token docs: https://developers.facebook.com/docs/facebook-login/guides/access-tokens/get-long-lived/
   - post docs: https://developers.facebook.com/docs/pages-api/posts/
   - insight docs: https://developers.facebook.com/docs/platforminsights/page
 */
 
-/*
-  Todo: Make everything super typesafe
-*/
-
 import { handleChannelApiCall } from "../utils/request";
 
-type GraphApiVersionType = "v19.0" | "v18.0" | "v17.0";
-
 export class FB {
-  constructor(fbAppId: string, fbAppSecret: string, graphApiVersion?: GraphApiVersionType) {
+  constructor() {}
+
+  /**
+   * Sets the fb graph api version
+   * @param graphApiVersion version of graph api for eg: v18.0, v19.0
+   * @default default value is v19.0
+   */
+  setFbGraphApiVersion(graphApiVersion: string) {
     //@ts-expect-error
-    this.graphApiVersion = graphApiVersion || "v19.0";
+    this.graphApiVersion = graphApiVersion;
+  }
+
+  /**
+   * Sets the fb app id
+   * @param fbAppId app id from meta developers dashboard
+   */
+  setFbAppId(fbAppId: string) {
     //@ts-expect-error
     this.fbAppId = fbAppId;
+  }
+
+  /**
+   * Sets the fb app secret
+   * @param fbAppSecret app secret from meta developers dashboard
+   */
+  setFbAppSecret(fbAppSecret: string) {
     //@ts-expect-error
     this.fbAppSecret = fbAppSecret;
   }
 
+  /**
+   * Create a Post
+   * @param pageId id of the page
+   * @param pageAccessToken  long lived access token of the page
+   * @returns id of the created post
+   */
   async createPost(pageId: string, body: string, pageAccessToken: string) {
     if (!pageId || !body || !pageAccessToken) {
       throw new Error("Not enough info to create post");
@@ -49,6 +70,12 @@ export class FB {
     return resp.id;
   }
 
+  /**
+   * Fetch latest 24 posts of the page
+   * @param pageId id of the page
+   * @param pageAccessToken long lived access token of the page
+   * @returns 24 latest posts of the specified page
+   */
   async getPagePosts(pageId: string, pageAccessToken: string) {
     if (!pageId || !pageAccessToken) {
       throw new Error("Not enough info to get page posts");
@@ -66,6 +93,11 @@ export class FB {
     return await response.json();
   }
 
+  /**
+   * Fetch user information
+   * @param shortLivedAccessToken access token retrieved from fb dialog modal
+   * @returns user's name and id, custom fields coming soon
+   */
   async getMe(shortLivedAccessToken: string) {
     if (!shortLivedAccessToken) {
       throw new Error("Invalid access token");
@@ -83,13 +115,18 @@ export class FB {
     return await response.json();
   }
 
+  /**
+   * Fetch long lived access token of the user
+   * @param shortLivedAccessToken access token retrieved from fb dialog modal
+   * @returns user's long lived access token
+   */
   async getUserLongLivedAccessToken(shortLivedAccessToken: string) {
-    if (!shortLivedAccessToken) {
-      throw new Error("Invalid access token");
-    }
-
     //@ts-expect-error
     const { fbAppId, fbAppSecret } = this;
+
+    if (!shortLivedAccessToken || !fbAppId || !fbAppSecret) {
+      throw new Error("Not enough info provided to fetch long lived access token.");
+    }
 
     const response = await handleChannelApiCall({
       endpoint: `oauth/access_token`,
@@ -111,6 +148,12 @@ export class FB {
     return longLivedAccessToken;
   }
 
+  /**
+   * Fetch first page information among all the access provided pages
+   * @param userLongLivedAccessToken long lived access token of the user
+   * @param appScopedUserId app id from the developers meta dashboard
+   * @returns first page information(id, name, long lived access tokens etc.) that it found or error if not
+   */
   async getFirstPage(userLongLivedAccessToken: string, appScopedUserId: string) {
     if (!userLongLivedAccessToken || !appScopedUserId) {
       throw new Error("Not enough data provided");
@@ -137,6 +180,12 @@ export class FB {
     return pages;
   }
 
+  /**
+   * Fetch analytics of the specified page, currently supported - page posts engagment
+   * @param pageId page id
+   * @param pageLongLivedAccessToken long lived access token of the page
+   * @returns insights of page posts engagement of last 30 days
+   */
   async getPageInsights(pageId: string, pageLongLivedAccessToken: string) {
     if (!pageId || !pageLongLivedAccessToken) {
       throw new Error("Not enough data provided");
